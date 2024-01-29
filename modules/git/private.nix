0ee64a0bfb13ed3d714129a3a901a94d33d6f64a
N1koge <rmrf.mc.npro.6672@gmail.com>
2024-01-29 21:42:19 +0000

    refactor: indentation

--- modules/git/private.nix
+++ modules/git/private.nix
@@ -3,19 +3,19 @@
 {
   programs.git = {
     enable = true;
-      userName = "N1koge";
-      userEmail = "rmrf.mc.npro.6672@gmail.com";
-      extraConfig = {
-        github.user = "N1koge";
-        core = {
-          sshCommand = "ssh -i ~/.ssh/github";
-          autocrlf = "input";
-        };
-        init.defaultBranch = "main";
-        pull = {
-          rebase = false;
-          ff = "only";
-        };
+    userName = "N1koge";
+    userEmail = "rmrf.mc.npro.6672@gmail.com";
+    extraConfig = {
+      github.user = "N1koge";
+      core = {
+        sshCommand = "ssh -i ~/.ssh/github";
+        autocrlf = "input";
       };
+      init.defaultBranch = "main";
+      pull = {
+        rebase = false;
+        ff = "only";
+      };
+    };
     };
 }
--- modules/git/work.nix
+++ modules/git/work.nix
@@ -3,19 +3,19 @@
 {
   programs.git = {
     enable = true;
-      userName = "";
-      userEmail = "";
-      extraConfig = {
-        github.user = "N1koge";
-        core = {
-          sshCommand = "ssh -i ~/.ssh/github";
-          autocrlf = "input";
-        };
-        init.defaultBranch = "main";
-        pull = {
-          rebase = false;
-          ff = "only";
-        };
+    userName = "";
+    userEmail = "";
+    extraConfig = {
+      github.user = "N1koge";
+      core = {
+        sshCommand = "ssh -i ~/.ssh/github";
+        autocrlf = "input";
       };
+      init.defaultBranch = "main";
+      pull = {
+        rebase = false;
+        ff = "only";
+      };
+    };
     };
 }
